896d00da0ff75130c74cabf29f2ed5535c7891dc
Jorge-Luques <jorgelqs79@gmail.com>
2020-07-28 00:47:31 -0300

    agrego usuarios desde archivo y pruebo bcrypt

--- cuenta.js
+++ cuenta.js
@@ -1,27 +1,72 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var fs = require("fs");
+var bcrypt = require('bcryptjs');
 var Admin = /** @class */ (function () {
     function Admin() {
         this.usuarios = [];
+        this.cargarUsuarios();
         this.cuentas = [];
     }
     Admin.prototype.addUsuario = function (user) {
         this.usuarios.push(user);
     };
-    Admin.prototype.quitarUsuario = function (user) {
-        var userAux;
-        var indexAux;
-        for (var i = 0; i < this.usuarios.length; i++) {
-            userAux = this.usuarios[i];
-            if (userAux.get_nickName === user.get_nickName) {
-                for (indexAux = i; indexAux < this.usuarios.length - 1; indexAux++) {
-                    this.usuarios[indexAux] = this.usuarios[indexAux + 1];
-                }
-                this.usuarios.pop();
-            }
-        }
-    };
+    // public quitarUsuario(user: Usuario): void{
+    //     let userAux: Usuario;
+    //     let indexAux: number;
+    //     for (let i=0; i < this.usuarios.length; i++){
+    //         userAux = this.usuarios[i];
+    //         if(userAux.get_nickName === user.get_nickName){
+    //             for(indexAux = i; indexAux < this.usuarios.length-1;indexAux++){
+    //                 this.usuarios[indexAux] = this.usuarios[indexAux+1];
+    //             }
+    //             this.usuarios.pop();
+    //         }
+    //     }
+    // }
     Admin.prototype.addcuenta = function (newCuenta) {
         this.cuentas.push(newCuenta);
         this.addUsuario(newCuenta.getUsuario());
+    };
+    Admin.prototype.cargarUsuarios = function () {
+        var texto = fs.readFileSync('usuarios.txt', 'utf-8');
+        this.usuarios = texto.split('\r\n');
     };
     return Admin;
 }());
@@ -42,12 +87,13 @@ var Usuario = /** @class */ (function () {
     return Usuario;
 }());
 var Cuenta = /** @class */ (function () {
+    // private salt= 10;
     function Cuenta(nick, password) {
         this.user = new Usuario(nick, password);
-        this.password = password;
+        this.password = "******";
     }
     Cuenta.prototype.getUsuario = function () {
-        return this.user;
+        return this.user.get_nickName();
     };
     Cuenta.prototype.setPassword = function (pass2) {
         this.password = pass2;
@@ -64,3 +110,48 @@ admin.addcuenta(miCta);
 console.log(admin);
 console.log(miUser);
 console.log(miCta);
+///probando bcrypt
+var myvalue = "JFDSNJDSNFJSDNFSJASASNDCAUDHANDKLAMDXIALWDMQAW";
+// bcrypt.genSalt(10, function(err, salt) {
+//     bcrypt.hash(myvalue, salt, function(err, hash) {
+//         console.log(myvalue);
+//         console.log(hash);
+//         console.log(bcrypt.compareSync(myvalue,hash));
+//     });
+// });
+// let salt = bcrypt.genSaltSync(10);
+// let hash = bcrypt.hashSync(myvalue,salt);
+// console.log(myvalue);
+// console.log(hash);
+// console.log(bcrypt.compareSync(myvalue,hash));
+bcrypt.hash(myvalue, 10, function (err, hash) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(hash);
+    bcrypt.compare(myvalue, hash, function (err, res) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(res);
+    });
+});
+var hashPassword = function () { return __awaiter(void 0, void 0, void 0, function () {
+    var hash, _a, _b;
+    return __generator(this, function (_c) {
+        switch (_c.label) {
+            case 0: return [4 /*yield*/, bcrypt.hash(myvalue, 10)];
+            case 1:
+                hash = _c.sent();
+                console.log(hash);
+                _b = (_a = console).log;
+                return [4 /*yield*/, bcrypt.compare(myvalue, hash)];
+            case 2:
+                _b.apply(_a, [_c.sent()]);
+                return [2 /*return*/];
+        }
+    });
+}); };
+hashPassword();
--- cuenta.ts
+++ cuenta.ts
@@ -1,33 +1,43 @@
+import * as rs from 'readline-sync';
+import * as fs from 'fs';
+const bcrypt = require('bcryptjs');
+
 class Admin{
-    private usuarios: Usuario[];
+    private usuarios: string[];
     private cuentas: Cuenta[];
     
     constructor (){
         this.usuarios = [];
+        this.cargarUsuarios();
         this.cuentas = []
     }
 
-    private addUsuario(user: Usuario):void{
+    private addUsuario(user: string):void{
         this.usuarios.push(user);
     }
 
-    public quitarUsuario(user: Usuario): void{
-        let userAux: Usuario;
-        let indexAux: number;
-        for (let i=0; i < this.usuarios.length; i++){
-            userAux = this.usuarios[i];
-            if(userAux.get_nickName === user.get_nickName){
-                for(indexAux = i; indexAux < this.usuarios.length-1;indexAux++){
-                    this.usuarios[indexAux] = this.usuarios[indexAux+1];
-                }
-                this.usuarios.pop();
-            }
-        }
-    }
+    // public quitarUsuario(user: Usuario): void{
+    //     let userAux: Usuario;
+    //     let indexAux: number;
+    //     for (let i=0; i < this.usuarios.length; i++){
+    //         userAux = this.usuarios[i];
+    //         if(userAux.get_nickName === user.get_nickName){
+    //             for(indexAux = i; indexAux < this.usuarios.length-1;indexAux++){
+    //                 this.usuarios[indexAux] = this.usuarios[indexAux+1];
+    //             }
+    //             this.usuarios.pop();
+    //         }
+    //     }
+    // }
 
     public addcuenta(newCuenta: Cuenta):void{
         this.cuentas.push(newCuenta);
         this.addUsuario(newCuenta.getUsuario());
+    }
+
+    private cargarUsuarios():void{
+        let texto: string = fs.readFileSync('usuarios.txt','utf-8');
+        this.usuarios = texto.split('\r\n');
     }
 
 }
@@ -63,14 +73,20 @@ class Usuario{
 class Cuenta{
     private user: Usuario;
     private password: string;
+    // private salt= 10;
+
+    
+    
 
     constructor (nick:string, password: string){
         this.user = new Usuario(nick, password);
-        this.password = password;
+
+        this.password="******";
+       
     }
 
-    public getUsuario():Usuario{
-        return this.user;
+    public getUsuario():string{
+        return this.user.get_nickName();
     }
 
     public setPassword(pass2: string):void{
@@ -85,8 +101,36 @@ class Cuenta{
 let admin:Admin = new Admin();
 let miUser: Usuario = new Usuario("jorge","pass01");
 let miCta: Cuenta = new Cuenta(miUser.get_nickName(),miUser.getContraseña());
+
 admin.addcuenta(miCta);
 
 console.log(admin);
 console.log(miUser);
 console.log(miCta);
+///probando bcrypt
+let myvalue = "JFDSNJDSNFJSDNFSJASASNDCAUDHANDKLAMDXIALWDMQAW";
+
+bcrypt.hash(myvalue, 10, (err, hash) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(hash)
+    
+    bcrypt.compare(myvalue, hash, (err, res) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(res)
+    })
+  
+  })
+  
+  const hashPassword = async () => {
+    const hash = await bcrypt.hash(myvalue, 10)
+    console.log(hash)
+    console.log(await bcrypt.compare(myvalue, hash))
+  }
+  
+  hashPassword();
